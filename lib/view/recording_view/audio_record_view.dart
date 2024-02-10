4e460f8f8206e4a0bcc9e5d631985c839992064b
Shubham1479 <vikivikings143@gmail.com>
2024-02-10 22:53:05 +0530

    feat:implementing audio frontend

---
+++ lib/view/recording_view/audio_record_view.dart
@@ -0,0 +1,26 @@
+//audio recording and viewing screen
+import 'package:flutter/material.dart';
+
+class AudioRecordingAndViewing extends StatefulWidget {
+  const AudioRecordingAndViewing({super.key});
+
+  @override
+  State<AudioRecordingAndViewing> createState() => _AudioRecordingAndViewingState();
+}
+
+class _AudioRecordingAndViewingState extends State<AudioRecordingAndViewing> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: buildStart(),
+      ),
+    );
+
+
+  Widget buildStart() {
+    final isRecordinng
+  }
+
+  }
+}
